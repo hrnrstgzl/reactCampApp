5668ed651e22998c07b364108ea6fb60c81933f9
Harun  Reşit Güzel <hrnrstgzl@gmail.com>
2021-06-06 00:59:00 +0300

    İlk gün sonu! Frontend'den datalar getirildi tabloya yüklendi

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
-import { Button, Container, Dropdown, Menu } from 'semantic-ui-react'
+import { Button, Container, Menu } from 'semantic-ui-react'
+import CartSummary from './CartSummary'
 export default function Navi() {
     return (
         <div>
-            <Menu inverted fixed size='tiny'>
+            <Menu inverted fixed="top" size='tiny'>
 
                 <Container>
-                <Menu.Item
-                    name='home'
-                />
-                <Menu.Item
-                    name='messages'
-                />
-
-                <Menu.Menu position='right'>
-                    <Dropdown item text='Language'>
-                        <Dropdown.Menu>
-                            <Dropdown.Item>English</Dropdown.Item>
-                            <Dropdown.Item>Russian</Dropdown.Item>
-                            <Dropdown.Item>Spanish</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-
-                    <Menu.Item>
-                        <Button primary>Sign Up</Button>
-                    </Menu.Item>
-                </Menu.Menu>
+                    <Menu.Item
+                        name='home'
+                    />
+                    <Menu.Item
+                        name='messages'
+                    />
+                    <Menu.Menu position='right'>
+                        <CartSummary />
+                        <Menu.Item>
+                            <Button primary>Sign Up</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
                 </Container>
             </Menu>
         </div>
