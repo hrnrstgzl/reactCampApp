da0919e721bf064e5f1457e4bfc76138c1c9e715
Harun  Reşit Güzel <hrnrstgzl@gmail.com>
2021-07-03 23:02:04 +0300

    ReactCamp Günsonu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import 'semantic-ui-css/semantic.min.css';
 import Dashboard from './layouts/Dashboard';
+import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react';
 import Navi from './layouts/Navi';
 
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Navi/>
-      <Container  className="main">
-      <Dashboard/>
-      </Container>
+      <Container className="main">
+         <Dashboard/>
+      </Container>    
     </div>
   );
 }
